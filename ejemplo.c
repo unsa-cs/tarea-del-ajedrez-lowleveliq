082a59f9b25f8235c6f9411d86cfdd6ab4ae6f00
ediazva <ediazva@unsa.edu.pe>
2024-10-04 17:30:54 -0500

    Primera solución del ejercicio 1 arreglando el error anterior retirando la funcion flipV que era innecesaria.

--- ejemplo.c
+++ ejemplo.c
@@ -1,11 +1,11 @@
 #include "chess.h"
 #include "figures.h"
+
 void display() {
   char **whiteblock = whiteSquare;
   char **blackblock = reverse(whiteSquare);
   char **bothblocks = join(whiteblock, blackblock);
   char **row = join(bothblocks, bothblocks);
-  char **flipped_row = flipV(row);
-  char **row2 = join(row, flipped_row);
+  char **row2 = join(row, row);
   interpreter(row2);
 }
