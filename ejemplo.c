ff9a95cf2cc5e31936636498f724ea9f91691568
Ferd1n4nd <fernando.fjcm.123@gmail.com>
2024-10-04 01:00:08 -0500

    Probando la liberación de la memoria en las variables BlackQueen y joined, liberación del doble puntero

--- ejemplo.c
+++ ejemplo.c
@@ -1,15 +1,10 @@
 #include "chess.h"
 #include "figures.h"
-
+#include <stdlib.h>
 void display(){
   char** BlackQueen = reverse(queen);
   char** joined = join(BlackQueen, pawn);
-  char** joined2 = join(king,rook);
-  char** repeat = repeatH(rook,10);
-  char** repeat2 = repeatV(repeat,8);
-  char** upp = up(rook,pawn);
-  char** upp2 = up(upp,knight);
-
-  interpreter(upp2);
-
+  interpreter(joined);
+  free(BlackQueen);
+  free(joined);
 }
