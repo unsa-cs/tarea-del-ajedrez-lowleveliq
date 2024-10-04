c07bc99be8a7550f99a68bd15116c339f1ef3f1b
Smilingvk <valeriaguzmanavalos@gmail.com>
2024-10-04 00:33:40 -0500

    Se probo las funcionalidades de repeatH , repeatV y up

--- ejemplo.c
+++ ejemplo.c
@@ -4,7 +4,12 @@
 void display(){
   char** BlackQueen = reverse(queen);
   char** joined = join(BlackQueen, pawn);
-  interpreter(BlackQueen);
-  interpreter(joined);
+  char** joined2 = join(king,rook);
+  char** repeat = repeatH(rook,10);
+  char** repeat2 = repeatV(repeat,8);
+  char** upp = up(rook,pawn);
+  char** upp2 = up(upp,knight);
+
+  interpreter(upp2);
 
 }
