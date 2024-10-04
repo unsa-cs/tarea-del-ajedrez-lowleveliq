bf7a2191fbb2b2d9bb10b281f37f576334e2f476
Saelemaekers <gonzarzp@gmail.com>
2024-10-04 16:55:30 -0500

    Se utilizo la funcion repeatH para crear una fila de 8 casillas blancas

--- ejemplo.c
+++ ejemplo.c
@@ -1,16 +1,7 @@
 #include "chess.h"
 #include "figures.h"
-#include <stdlib.h>
 void display(){
-  char** background = reverse(whiteSquare);
-  char** super = superImpose(knight, background);
-  char** doble = repeatH(super,2);
-  char** rotate = rotateL(doble);
-  char** flip = flipV(rotate);
-  interpreter(flip);
-  free(background);
-  free(super);
-  free(rotate);
-  free(doble);
-  free(flip);
+  char** whiteblock = whiteSquare;
+  char** block = repeatH(whiteblock, 8);
+  interpreter(block);
 }
