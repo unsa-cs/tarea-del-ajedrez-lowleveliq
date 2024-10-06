7935435d41a8f782967fc46ed09cb1a432c6f842
Saelemaekers <gonzarzp@gmail.com>
2024-10-05 23:43:19 -0500

    Se cambio el orden de los argumentos empezando por la pieza y luego la casilla y funciono como se deseaba, adicionalmente cambiamos la pieza de -pawn- a -rook-

--- prob_04.c
+++ prob_04.c
@@ -4,6 +4,6 @@
 void display() {
   char **blackSquare = reverse(whiteSquare);
   char **bothblocks = join(blackSquare, whiteSquare);
-  char **Peon = superImpose(blackSquare, pawn);
+  char **Peon = superImpose(rook, blackSquare);
   interpreter(Peon);
 }
