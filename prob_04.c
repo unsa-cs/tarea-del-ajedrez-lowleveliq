f1da39f1f399f9f690dfb195675c59dd5b669609
Saelemaekers <gonzarzp@gmail.com>
2024-10-06 00:23:22 -0500

    Quizimos hacer una prueba mas para entender mejor el comportamiento de superImpose, descubriendo que este no solo sobrepone primera imagen sobre la segunda, sino que tambien la recorta al tamaño de la imagen mas pequeña, por lo que para conseguir el comportamiento esperado ambas imagenes deben ser del mismo tamaño.

--- prob_04.c
+++ prob_04.c
@@ -4,7 +4,8 @@
 void display() {
   char **blackSquare = reverse(whiteSquare);
   char **bothblocks = join(blackSquare, whiteSquare);
-  char **row = repeatH(bothblocks, 4);
-  char **rowrook = superImpose(rook, row);
-  interpreter(rowrook);
+  char **twoKnights = repeatH(knight, 2);
+  char **knightblocks = superImpose(twoKnights, bothblocks);
+  interpreter(knightblocks);
 }
+
