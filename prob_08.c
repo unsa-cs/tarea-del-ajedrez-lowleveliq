50e0830cf6e1f8d6ee0a66a7c348ba9c7845c7ee
ediazva <ediazva@unsa.edu.pe>
2024-10-06 02:20:24 -0500

    Se está avanzando el desarrollo del ejercicio 8, para facilitar la combinación de los patrones de cada fila, se creó la inversa de la unión de los bloques negro y blanco.

--- prob_08.c
+++ prob_08.c
@@ -4,17 +4,18 @@
 void display() {
   char **blackSquare = reverse(whiteSquare);
   char **bothblocks = join(blackSquare, whiteSquare);
+  char **bothIblocks = reverse(bothblocks);
   char **queenBlack = superImpose(queen, blackSquare);
   char **queenWhite = superImpose(queen, whiteSquare);
 
-  char **row1 = join(bothblocks, join(queenBlack, join(whiteSquare, repeatH(bothblocks, 2))));  
-  char **row2 = join(queenWhite, join(blackSquare, repeatH(bothblocks, 3)));  
-  char **row3;  
-  char **row4;  
-  char **row5;  
+  char **row1 = join(queenBlack, join(whiteSquare, repeatH(bothblocks, 3)));  
+  char **row2 = join(repeatH(bothIblocks, 2), join(queenWhite, join(blackSquare, bothIblocks)));  
+  char **row3 = join(repeatH(bothblocks, 3), join(blackSquare,  queenWhite));  
+  char **row4 = join(repeatH(bothIblocks, 2), join(whiteSquare, join(queenBlack, bothIblocks))); 
+  char **row5 = join(bothblocks, join(queenBlack, join(whiteSquare, repeatH(bothblocks, 2))));  
   char **row6;  
   char **row7;  
   char **row8;  
 
-  interpreter(row2);
+  interpreter(row5);
 }
