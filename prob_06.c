d9fce2a719f3f9bb740d6b3bb34e7f678ae38d9e
All23tor <acc.dummyagta1@gmail.com>
2024-10-06 01:01:33 -0500

    Este código produce un segmentation fault, parece ser que el llamar superImpose en dos imagene de distinta altura provoca esto.

---
+++ prob_06.c
@@ -0,0 +1,21 @@
+
+#include "chess.h"
+#include "figures.h"
+
+void display() {
+  char **blackSquare = reverse(whiteSquare);
+  char **bothblocks = join(blackSquare, whiteSquare);
+  char **row = repeatH(bothblocks, 4);
+  char **piezas = join(rook, knight);
+  piezas = join(piezas, bishop);
+  piezas = join(piezas, queen);
+  piezas = join(piezas, king);
+  piezas = join(piezas, bishop);
+  piezas = join(piezas, knight);
+  piezas = join(piezas, rook);
+  char **peones = repeatH(pawn, 8);
+  piezas = up(piezas, peones);
+  char **fullrow = superImpose(piezas, row);
+
+  interpreter(fullrow);
+}
