9ce6ef9a903e1f6f34c03b15e1efa85c29c314d9
ediazva <ediazva@unsa.edu.pe>
2024-10-06 02:08:18 -0500

    Se intentó realizar el patrón de la segunda fila de la solución a las 8 reinas pero al unir las diferentes imagenes hubo un error en la continuidad de colores.

--- prob_08.c
+++ prob_08.c
@@ -4,10 +4,17 @@
 void display() {
   char **blackSquare = reverse(whiteSquare);
   char **bothblocks = join(blackSquare, whiteSquare);
-  char **oddrow = repeatH(bothblocks,4);
-  char **evenrow = reverse(oddrow);
   char **queenBlack = superImpose(queen, blackSquare);
   char **queenWhite = superImpose(queen, whiteSquare);
 
-  interpreter(join(queenBlack, queenWhite));
+  char **row1 = join(bothblocks, join(queenBlack, join(whiteSquare, repeatH(bothblocks, 2))));  
+  char **row2 = join(queenWhite, join(blackSquare, repeatH(bothblocks, 3)));  
+  char **row3;  
+  char **row4;  
+  char **row5;  
+  char **row6;  
+  char **row7;  
+  char **row8;  
+
+  interpreter(row2);
 }
