88c25194d25d671493ff57d94d9b54d394cecbe4
ediazva <ediazva@unsa.edu.pe>
2024-10-06 02:28:14 -0500

    Se completó el desarrollo del ejercicio 8 realizando el patrón de cada fila y uniendolas mediante la función up.

--- prob_08.c
+++ prob_08.c
@@ -13,9 +13,9 @@ void display() {
   char **row3 = join(repeatH(bothblocks, 3), join(blackSquare,  queenWhite));  
   char **row4 = join(repeatH(bothIblocks, 2), join(whiteSquare, join(queenBlack, bothIblocks))); 
   char **row5 = join(bothblocks, join(queenBlack, join(whiteSquare, repeatH(bothblocks, 2))));  
-  char **row6;  
-  char **row7;  
-  char **row8;  
+  char **row6 = join(repeatH(bothIblocks, 3), join(queenWhite, blackSquare));  
+  char **row7 = join(blackSquare, join(queenWhite, repeatH(bothblocks, 3)));  
+  char **row8 = join(bothIblocks, join(whiteSquare, join(queenBlack, repeatH(bothIblocks, 2))));  
 
-  interpreter(row5);
+  interpreter(up(row1, up(row2, up(row3, up(row4, up(row5, up(row6, up(row7, row8))))))));
 }
