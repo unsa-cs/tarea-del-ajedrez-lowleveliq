80585f05d3e7907237242b9cebd832336a732a3e
Smilingvk <valeriaguzmanavalos@gmail.com>
2024-10-06 00:36:33 -0500

    Intentando unir varias piezas para resolver el problema 4 ,llamando join repetidas veces

--- prob_04.c
+++ prob_04.c
@@ -4,8 +4,14 @@
 void display() {
   char **blackSquare = reverse(whiteSquare);
   char **bothblocks = join(blackSquare, whiteSquare);
-  char **twoKnights = repeatH(knight, 2);
-  char **knightblocks = superImpose(twoKnights, bothblocks);
-  interpreter(knightblocks);
+  char **piezas = join(rook,knight);
+  piezas = join(piezas,bishop);
+  piezas = join(piezas,queen);
+  piezas = join(piezas,king);
+  piezas = join(piezas,bishop);
+  piezas = join(piezas,knight);
+  piezas = join(piezas,rook);
+
+  interpreter(piezas);
 }
 
