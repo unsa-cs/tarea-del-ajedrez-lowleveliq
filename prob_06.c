f806503cf5ff8d17b037a15a1ccaae0541031272
All23tor <acc.dummyagta1@gmail.com>
2024-10-06 01:14:02 -0500

    Uniendo todo el progreso hasta ahora se consiguió resolver el ejercicio 6, concatenando todas las partes separadas con la función up.

--- prob_06.c
+++ prob_06.c
@@ -8,6 +8,7 @@ void display() {
   char **oddRow = repeatH(bothblocks, 4);
   char **evenRow = reverse(oddRow);
   char **twoRows = up(oddRow, evenRow);
+
   char **piezas = join(rook, knight);
   piezas = join(piezas, bishop);
   piezas = join(piezas, queen);
@@ -18,9 +19,11 @@ void display() {
   char **peones = repeatH(pawn, 8);
   char **piezasBlancas = up(piezas, peones);
   char **piezasNegras = reverse(up(peones, piezas));
-  char **fullWhiteRow = superImpose(piezasBlancas, twoRows);
-  char **fullBlackRow = superImpose(piezasNegras, twoRows);
-  char **middleGround = repeatV(twoRows, 2);
 
-  interpreter(middleGround);
+  char **fullWhiteRows = superImpose(piezasBlancas, twoRows);
+  char **fullBlackRows = superImpose(piezasNegras, twoRows);
+  char **middleGround = repeatV(twoRows, 2);
+  char **fullBoard = up(fullWhiteRows, up(middleGround, fullBlackRows));
+
+  interpreter(fullBoard);
 }
