c779bfffb352decbfc24df67dacd818945272b39
Saelemaekers <gonzarzp@gmail.com>
2024-10-06 00:13:23 -0500

    Esperabamos que superImpose nos retorne una fila con una torre en la primera casilla; sin embargo, unicamente retorno una casilla con la torre, sin el resto de la fila que deseabamos

--- prob_04.c
+++ prob_04.c
@@ -5,8 +5,6 @@ void display() {
   char **blackSquare = reverse(whiteSquare);
   char **bothblocks = join(blackSquare, whiteSquare);
   char **row = repeatH(bothblocks, 4);
-  char **Torre = superImpose(rook, blackSquare);
-  char **completeblock = join(bothblocks, Torre);
-  char **completerow = join(row, completeblock);
-  interpreter(completerow);
+  char **rowrook = superImpose(rook, row);
+  interpreter(rowrook);
 }
